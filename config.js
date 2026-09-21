a1cc65e5b09845734d3a4441bf6c739c5a0ca440
hihihikj <30769517+hihihikj@users.noreply.github.com>
2026-09-21 20:47:00 +0200

    Switch to Supabase Auth and RLS

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.ENGLISH_GYM_CONFIG = {
-  version: "1.0",
-  apiBase: "https://seqoovfrpshsyqasxcqc.supabase.co/functions/v1/english-gym",
+  version: "1.1",
+  supabaseUrl: "https://seqoovfrpshsyqasxcqc.supabase.co",
+  publishableKey: "sb_publishable_G4qIx-xFCmkyYDT3eWybeg_2O2rJLlK",
   paidApisEnabled: false,
   aiEnabled: false
 };
