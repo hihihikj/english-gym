f1abfab28217ea5e656e6cdd80ca4852b74b61ad
hihihikj <30769517+hihihikj@users.noreply.github.com>
2026-09-22 23:27:22 +0200

    Bump English Gym to v1.2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ENGLISH_GYM_CONFIG = {
-  version: "1.1",
+  version: "1.2",
   supabaseUrl: "https://seqoovfrpshsyqasxcqc.supabase.co",
   publishableKey: "sb_publishable_G4qIx-xFCmkyYDT3eWybeg_2O2rJLlK",
   paidApisEnabled: false,
